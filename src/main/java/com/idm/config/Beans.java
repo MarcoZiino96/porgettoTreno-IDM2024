6de089bf2c22146327b1620d2286e03fb2426241
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-07-30 18:09:00 +0200

    conflitti risolti

--- src/main/java/com/idm/config/Beans.java
+++ src/main/java/com/idm/config/Beans.java
@@ -1,7 +1,6 @@
 package com.idm.config;
 
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,21 +13,16 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-<<<<<<< HEAD
 import com.idm.dao.PrenotazioneDao;
 import com.idm.dao.VagonePasseggeriDao;
 import com.idm.dao.VotoDao;
 import com.idm.dao.impl.PrenotazioneDaoImpl;
-=======
 import com.idm.dao.LocomotivaDao;
 import com.idm.dao.TrenoDao;
 import com.idm.dao.UtenteDao;
-import com.idm.dao.VagonePasseggeriDao;
 import com.idm.dao.impl.LocomotivaDaoImpl;
 import com.idm.dao.impl.TrenoDaoImpl;
 import com.idm.dao.impl.UtenteDaoImpl;
->>>>>>> 22422b7e170019fd50518133ae4fb0fdd5425195
 import com.idm.dao.impl.VagonePasseggeriImpl;
 import com.idm.dao.impl.VotoDaoImpl;
 
@@ -38,7 +32,6 @@ import com.idm.dao.impl.VotoDaoImpl;
 @EnableTransactionManagement
 
 public class Beans {
-<<<<<<< HEAD
 	
 	    @Value("${spring.datasource.url}")
 	    private String url;
@@ -82,45 +75,6 @@ private HibernateJpaVendorAdapter getJpaVendorAdapter() {
 	adapter.setShowSql(true);               
 	return adapter;
 }	
-=======
-
-	@Bean(name="dataSource")
-	public DataSource getDataSource () {
-
-		DriverManagerDataSource ds = new DriverManagerDataSource(); 
-		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		ds.setUsername("root");
-		ds.setPassword("root");
-		ds.setUrl("jdbc:mysql://localhost:3306/prova_treno");
-		return ds; 
-	} 
-
-	@Bean(name="entityManager")
-	public LocalContainerEntityManagerFactoryBean  getEntityManager(){
-
-		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-		// JDBC
-		factory.setDataSource(getDataSource());  
-
-		// imposta il dialogo tra JPA e hibernate
-		factory.setJpaVendorAdapter(getJpaVendorAdapter()); // imposta il dialogo tra JPA e hibernate
-
-		// impostare il luogo dove si trovano i bean (entità del DB)
-		//factory.setPackagesToScan(this.getClass().getPackage().getName()); 
-		factory.setPackagesToScan("com.idm.entity"); 
-		return factory; 
-	} 	
-
-
-	private HibernateJpaVendorAdapter getJpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);   // obbligatorio: serve per tradurre le query nel particolare Dialetto
->>>>>>> 22422b7e170019fd50518133ae4fb0fdd5425195
-
-		adapter.setGenerateDdl(true);          //facoltativo, attiva il DDL cio� hibernate creaer� le strutture nel DB se non sono gi� essitenti
-		adapter.setShowSql(false);              // mostra l'SQL, comodo per i corsi e per il debug ma in produzione solitamente � a false 
-		return adapter;
-	}	
 
 	/**** transazioni ****/
 	@Bean
@@ -130,12 +84,7 @@ private HibernateJpaVendorAdapter getJpaVendorAdapter() {
 		return transactionManager;
 	}
 
-<<<<<<< HEAD
-@Bean(name="vagonePasseggeri") 
-public VagonePasseggeriDao getCategoriaDao (){
-	VagonePasseggeriDao dao = new VagonePasseggeriImpl();
-	   return dao; 
-}
+
 @Bean(name="prenotazione") 
 public PrenotazioneDao getPrenotazione (){
 	PrenotazioneDao dao = new PrenotazioneDaoImpl();
@@ -146,7 +95,7 @@ public VotoDao getVoto (){
 	VotoDao dao = new VotoDaoImpl();
 	   return dao; 
 }
-=======
+
 	///**** sezione DAO ****/
 
 	@Bean(name="vagonePasseggeri") 
@@ -173,6 +122,5 @@ public VotoDao getVoto (){
 		LocomotivaDao dao = new LocomotivaDaoImpl();
 		return dao;
 	}
->>>>>>> 22422b7e170019fd50518133ae4fb0fdd5425195
 
 }
